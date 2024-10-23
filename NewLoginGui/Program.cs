34afc162595da2f8106b06d59894af6e58447715
WhatAPity171 <153460762+WhatAPity171@users.noreply.github.com>
2024-10-23 12:57:08 +0200

    Class for Quizes in Program.cs

--- NewLoginGui/Program.cs
+++ NewLoginGui/Program.cs
@@ -2,6 +2,21 @@ namespace NewLoginGui
 {
     internal static class Program
     {
+        struct Answer
+        {
+            string q;
+            bool isTrue;
+        }
+        class Quiz
+        {
+            string quest;
+            bool hasPicture;
+            Answer[] ans = new Answer[4];
+            public Quiz(string quest)
+            {
+                this.quest = quest;
+            }
+        }
         [STAThread]
         static void Main()
         {
