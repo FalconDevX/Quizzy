8fc6344bc618eac0f18eea3f4088c1191c83af34
Mateusz Nowaczek <mateusznowaczekmojekonto@gmail.com>
2024-11-11 21:19:28 +0100

    Adding Screenshots folder

--- WPF/obj/Debug/net8.0-windows/MainScreen.g.i.cs
+++ WPF/obj/Debug/net8.0-windows/MainScreen.g.i.cs
@@ -301,7 +301,7 @@ namespace WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF;V1.0.0.0;component/mainscreen.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF;component/mainscreen.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainScreen.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
